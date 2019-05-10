baa0b2cd640ea235a520f6aac91312cfe81735fc
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-05-10 10:46:55 +0530

    Create length_breadth_area.java

---
+++ length_breadth_area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double l=sc.nextDouble();
+        double b=sc.nextDouble();
+        double result=l*b;
+        System.out.printf("%.5f",result);
+        
+    }
+}
