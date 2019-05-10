da4102db1af3e7e5b5d932a5b06488a80d7fc185
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-05-10 10:28:59 +0530

    Create differenc between numbers.java

---
+++ differenc between numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int result=n-m;
+        System.out.println(Math.abs(result));
+    }
+}
